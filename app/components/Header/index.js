f6770a77fa3b747d12f86157305e55d27c80e201
ALEKSANDR BASALILOV <aleks@ALEKSANDRs-MacBook-Pro.local>
2017-10-27 14:15:36 -0700

    updated for presentation

---
+++ app/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import A from './A';
+import Img from './Img';
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
+import Banner from './banner.png';
+import messages from './messages';
+
+class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div  className="headerLink1" style={{marginTop:'3rem'}}>
+          <Img src={Banner} alt="react-boilerplate - Logo" />
+        <NavBar>
+          <HeaderLink  to="/">
+            <FormattedMessage {...messages.home} />
+          </HeaderLink>
+          <HeaderLink className="headerLink2" to="/concepts">
+            <FormattedMessage {...messages.features} />
+          </HeaderLink>
+        </NavBar>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ app/containers/HomePage/index.js
@@ -0,0 +1,135 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+
+import injectReducer from 'utils/injectReducer';
+import injectSaga from 'utils/injectSaga';
+import { makeSelectRepos, makeSelectLoading, makeSelectError } from 'containers/App/selectors';
+import H2 from 'components/H2';
+import ReposList from 'components/ReposList';
+import AtPrefix from './AtPrefix';
+import CenteredSection from './CenteredSection';
+import Form from './Form';
+import Input from './Input';
+import Section from './Section';
+import messages from './messages';
+import { loadRepos } from '../App/actions';
+import { changeUsername } from './actions';
+import { makeSelectUsername } from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  /**
+   * when initial state username is not null, submit the form to load repos
+   */
+
+   constructor(props){
+      super(props)
+      this.state = {
+        inputText : ''
+      }
+      this.updateInputText = this.updateInputText.bind(this)
+   }
+  componentDidMount() {
+    if (this.props.username && this.props.username.trim().length > 0) {
+      this.props.onSubmitForm();
+    }
+  }
+
+  updateInputText(e){
+    this.setState({inputText:e.target.value})
+  }
+
+  render() {
+    const { loading, error, repos } = this.props;
+    const reposListProps = {
+      loading,
+      error,
+      repos,
+    };
+
+    return (
+      <article>
+        <Helmet>
+          <title>A Cypress Demo</title>
+          <meta name="description" content="A HomeWiseDocs Cypress Demo" />
+        </Helmet>
+        <div>
+          <CenteredSection>
+            <H2>
+              Cypress Demo
+            </H2>
+          </CenteredSection>
+          <Section>
+            <H2>
+              Change this text
+            </H2>
+            <input className='action-input' placeholder='test the input here' style={{border:'solid black 2px', width:'20rem'}} onChange={this.updateInputText}/>
+          </Section>
+          <div >
+            <H2>
+              Component State
+            </H2>
+            <div style={{border:'solid black 2px', height:'3rem'}}>
+            {this.state.inputText}
+            </div>
+          </div>
+        </div>
+      </article>
+    );
+  }
+}
+
+HomePage.propTypes = {
+  loading: PropTypes.bool,
+  error: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.bool,
+  ]),
+  repos: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.bool,
+  ]),
+  onSubmitForm: PropTypes.func,
+  username: PropTypes.string,
+  onChangeUsername: PropTypes.func,
+};
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    onChangeUsername: (evt) => dispatch(changeUsername(evt.target.value)),
+    onSubmitForm: (evt) => {
+      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+      dispatch(loadRepos());
+    },
+  };
+}
+
+const mapStateToProps = createStructuredSelector({
+  repos: makeSelectRepos(),
+  username: makeSelectUsername(),
+  loading: makeSelectLoading(),
+  error: makeSelectError(),
+});
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'home', reducer });
+const withSaga = injectSaga({ key: 'home', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(HomePage);
